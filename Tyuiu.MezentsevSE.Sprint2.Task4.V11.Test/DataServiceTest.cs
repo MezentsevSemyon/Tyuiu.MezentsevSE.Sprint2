3c0e78c49f85bbb82b8d5f4eb95ec42f86cd4b6e
Tyuiu.MezentsevSE@outlook.com <Tyuiu.MezentsevSE@outlook.com>
2023-10-15 15:04:09 +0500

    Task4

---
+++ Tyuiu.MezentsevSE.Sprint2.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MezentsevSE.Sprint2.Task4.V11.Lib;
+
+namespace Tyuiu.MezentsevSE.Sprint2.Task4.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 121;
+            Assert.AreEqual(wait, res);
+
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = - 156;
+            double res = ds.Calculate(x, y);
+            double wait = -155.987;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MezentsevSE.Sprint2.Task4.V11/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MezentsevSE.Sprint2.Task4.V11.Lib;
+
+namespace Tyuiu.MezentsevSE.Sprint2.Task4.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1| Выполнил: Мезенцев С.Е. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил Мезенцев Семён Евгеньевич| ИИПб-23-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу, которая вычисляет требуемое значение с использованием*");
+            Console.WriteLine("*тернарного оператора                                                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение х:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+
+            double res = ds.Calculate(x, y);
+
+
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции =" + res);
+
+            Console.ReadKey();
+        }
+    }
+}
